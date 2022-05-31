a59d850297ded19b1af00094fcf5aa4e31940bd4
AhmadAbdallah13 <a7mtoo133@gmail.com>
2022-06-01 02:37:12 +0300

    first commit

---
+++ Transactions.py
@@ -0,0 +1,16 @@
+import uuid
+import time
+
+
+class Transaction:
+    def __init__(self, sender_public_key, receiver_public_key, amount, transaction_type):
+        self.senderPublicKey = sender_public_key
+        self.receiver_public_key = receiver_public_key
+        self.amount = amount
+        self.transaction_type = transaction_type
+        self.id = uuid.uuid1().hex
+        self.timestamp = time.time()
+        self.signature = ""
+
+    def to_json(self):
+        return self.__dict__
---
+++ main.py
@@ -0,0 +1,11 @@
+from Transactions import Transaction
+
+
+if __name__ == "__main__":
+    sender = "sender"
+    receiver = "receiver"
+    amount = 1
+    transaction_type = "TRANSFER"
+    
+    transaction = Transaction(sender, receiver, amount, transaction_type)
+    print(transaction.to_json())
