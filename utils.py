d9e484191736e59a0b7a2ca8f2e54018f7dac72f
AhmadAbdallah13 <a7mtoo133@gmail.com>
2022-06-02 20:31:36 +0300

    add utils class

---
+++ utils.py
@@ -0,0 +1,12 @@
+import json
+
+from Crypto.Hash import SHA256
+
+
+class Utils:
+    @staticmethod
+    def hash(data):
+        data_string = json.dumps(data)
+        data_bytes = data_string.encode("utf-8")
+        hashed_data = SHA256.new(data_bytes)
+        return hashed_data
